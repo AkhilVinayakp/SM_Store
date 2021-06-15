61e1a071ddc49891936369a7a1a25379277a6d32
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-06-15 17:05:46 +0530

    Handling the medias

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# imports to add media directory to the url
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls'))
 ]
+
+# adding the media url and root, add it as a static path
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
