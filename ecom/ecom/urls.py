6f5c1a79cbf35ee2a4b64939d975d214a862a669
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-06-25 15:49:26 +0530

    Project structure Updated

--- ecom/ecom/urls.py
+++ ecom/ecom/urls.py
@@ -22,7 +22,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include('api.urls'))
 ]
 
 # adding the media url and root, add it as a static path
