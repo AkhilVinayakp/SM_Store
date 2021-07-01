035f174f20e51c0fad75ae7b7555134ac875c0fb
AkhilVinayakp <nirvanaosborn@gmail.com>
2021-07-01 16:10:14 +0530

    path set

--- ecom/api/category/views.py
+++ ecom/api/category/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+# we need our model
+from .models import Category
+# importing the corresponding serializer
+from .serializers import CategorySerializer
+
+
+class CategoryViewSet(viewsets.ViewSet):
+    """specifying the query to pull all the Category model data"""
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -4,5 +4,7 @@ from rest_framework.authtoken import views
 from .views import home
 
 urlpatterns = [
-    path("", home, name="api.home")
+    path("", home, name="api.home"),
+    # adding the category path with it's own urls
+    path("category/", include('api.category.urls')),
 ]
